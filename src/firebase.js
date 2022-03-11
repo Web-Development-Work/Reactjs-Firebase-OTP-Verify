2c0be7077fecfb5e8ef199ba35a89003d6083bb2
Anil Patidar <patidaranil0791@gmail.com>
2022-03-11 12:13:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyARdfAkrPhND48eSiBmdGhhtiAIwfRk3t4",
-  authDomain: "otp-verify-4263e.firebaseapp.com",
-  projectId: "otp-verify-4263e",
-  storageBucket: "otp-verify-4263e.appspot.com",
-  messagingSenderId: "645498443309",
-  appId: "1:645498443309:web:a973e127901f5bcb528aa4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
